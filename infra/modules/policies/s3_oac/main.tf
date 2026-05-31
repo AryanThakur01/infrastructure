8c8192b991edb6940581d8aa349b0417f8c445be
Aryan Thakur <aryan197297@gmail.com>
2026-05-31 16:32:35 +0530

    update(s3-oac): separate outputs from main

--- infra/modules/policies/s3_oac/main.tf
+++ infra/modules/policies/s3_oac/main.tf
@@ -42,8 +42,3 @@ data "aws_iam_policy_document" "oac" {
     }
   }
 }
-
-output "s3_bucket_oac_policy" {
-  description = "The IAM policy document that grants CloudFront OAC access to the S3 bucket."
-  value       = data.aws_iam_policy_document.oac.json
-}
