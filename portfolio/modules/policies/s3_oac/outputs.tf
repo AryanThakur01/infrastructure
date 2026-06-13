e7c7d02536dffc17ac427cd36c23aba254cf2f19
Aryan Thakur <aryan197297@gmail.com>
2026-06-13 19:45:04 +0530

    update(cleanup): remove modules from setup

--- portfolio/modules/policies/s3_oac/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_oac_policy" {
-  description = "The IAM policy document that grants CloudFront OAC access to the S3 bucket."
-  value       = data.aws_iam_policy_document.oac.json
-}
