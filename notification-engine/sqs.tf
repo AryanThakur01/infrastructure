710a58a61d4689527667d8ed4a66b7ed5be019ea
Aryan Thakur <aryanthakur@Aryans-MacBook-Air.local>
2026-07-05 17:29:02 +0530

    update(sqs): concurrent requests

--- notification-engine/sqs.tf
+++ notification-engine/sqs.tf
@@ -1,15 +1,15 @@
 locals {
   queues = {
     high = {
-      max_concurrency    = 10
+      max_concurrency    = 3
       visibility_timeout = 30
     }
     medium = {
-      max_concurrency    = 5
+      max_concurrency    = 2
       visibility_timeout = 30
     }
     low = {
-      max_concurrency    = 2
+      max_concurrency    = 1
       visibility_timeout = 30
     }
   }
